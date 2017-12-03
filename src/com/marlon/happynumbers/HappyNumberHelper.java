a6952e5c789c350bd7f84ca40379adf3e68e71bb
Marlon Jakobsson <jakobssonmarlon@gmail.com>
2017-12-03 12:22:20 +0100

    added comments

--- src/com/marlon/happynumbers/HappyNumberHelper.java
+++ src/com/marlon/happynumbers/HappyNumberHelper.java
@@ -8,11 +8,11 @@ public class HappyNumberHelper {
 		
 		ArrayList<Integer> numbersChecked = new ArrayList<>();
 		
+		// Om ett nummer som redan existerar i listan kommer in visar det på att det är en loop => unhappy number
 		while(!numbersChecked.contains(number)){
 			
 			numbersChecked.add(number);			
-			int sum = calculateSum(number);			
-			number = sum;
+			number = calculateSum(number);			
 			
 			if (number == 1) {				
 				return true;				
@@ -20,15 +20,69 @@ public class HappyNumberHelper {
 		}		
 		return false;		
 	}
-
-	private int calculateSum(int number) {
+	
+	private static int calculateSum(int number) {
 		
+		/* Plocka ut varje enskilt tal i number: 
+		 * Första talet går att få ut genom enkel division då int helt enkelt tar bort resterande decimaler. 
+		 * Sista numret är alltid number % 10
+		 * De mellersta talen tas ut genom att ta bort de siffror så att talet man är ute efter nu ska vara sista talet. => % 10 
+		 */
+		
+		int length = String.valueOf(number).length();
+		int firstNumber;
+		int secondNumber;
+		int thirdNumber;
+		int fourthNumber;
 		int sum = 0;
 		
-		while(number > 0) {			
-			sum += (number % 10) * (number % 10);
-			number = number / 10;
-		}		
-		return sum;
+		if (length == 1) {
+			sum = number * number;
+			return sum;
+		}
+		if (length == 2) {
+			firstNumber = number / 10;
+			secondNumber = number % 10;
+			sum = firstNumber * firstNumber;
+			sum += secondNumber * secondNumber;			
+			return sum;			
+		}
+		if (length == 3) {
+			firstNumber = number / 100;
+			secondNumber = number / 10;
+			secondNumber %= 10;
+			thirdNumber = number % 10; 			
+			sum = firstNumber * firstNumber;
+			sum += secondNumber * secondNumber;	
+			sum += thirdNumber * thirdNumber;					
+			return sum;	
+		}
+		if (length == 4) {
+			firstNumber = number / 1000;
+			secondNumber = number / 100;
+			secondNumber %= 10;
+			thirdNumber = number / 10;
+			thirdNumber %= 10; 
+			fourthNumber = number % 10;			
+			sum = firstNumber * firstNumber;
+			sum += secondNumber * secondNumber;	
+			sum += thirdNumber * thirdNumber;
+			sum += fourthNumber * fourthNumber;					
+			return sum;	
+		}
+		return 0;
 	}
+	
+	// Mer effektiv metod jag senare hittade
+
+//	private int calculateSum(int number) {
+//		
+//		int sum = 0;
+//		
+//		while(number > 0) {			
+//			sum += (number % 10) * (number % 10);
+//			number = number / 10;
+//		}		
+//		return sum;
+//	}
 }
--- src/com/marlon/happynumbers/HappyNumbers.java
+++ src/com/marlon/happynumbers/HappyNumbers.java
@@ -6,14 +6,19 @@ public class HappyNumbers {
 	public static void main(String[] args) {
 		
 		HappyNumberHelper helper = new HappyNumberHelper();
+		int counter = 0;
 	
+		//Loop som går igenom talen 1-1000
 		for(int i = 1; i <= 1000; i++) {
 			
 			boolean isNumberHappy = helper.checkIfNumberIsHappy(i);
 			
 			if (isNumberHappy) {				
-				System.out.println(i);				
+				System.out.println(i);	
+				counter++;
 			}
-		}		
+		}
+		
+		System.out.println("Total number of happy numbers : " + counter);
 	}
 }
